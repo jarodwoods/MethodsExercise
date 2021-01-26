2e661de8a9e47d8cd78c0ff1a0cbe1c583f7ea04
Jarod L Woods <jarodlwoods@gmail.com>
2021-01-26 09:54:23 -0600

    exercise 2

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace MethodsExercise
 {
+    //[DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hi what's your name");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hi, {name}!  How are you?");
+            Console.WriteLine("Hi what's your favortie color");
+            var color = Console.ReadLine();
+            Console.WriteLine($"Hi, {color}!");
+            Console.WriteLine("Whats your favorite animal?");
+            var animal = Console.ReadLine();
+            Console.WriteLine($"Favorite Animal: {animal}");
+            Console.WriteLine("Whats your favorite band?");
+            var band = Console.ReadLine();
+            Console.WriteLine($"Favorite Band: {band}");
+            //Exercise ll
             Console.WriteLine("Hello World!");
+            var sum = Console.ReadLine();
+
+            var results = Add(10, 20);
+            var times = Multiply(10, 20);
+        }
+        public static int Add(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        }
+        public static int Multiply(int num1, int num2)
+        {
+            var answer = num1 * num2;
+            return answer;
+
+
         }
     }
 }
+
+
---
+++ MethodsExercise/ProgramBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace MethodsExercise
+{
+    
+    internal abstract class ProgramBase
+    {
+
+       // var amount = Sum(2, 6);
+
+       // public abstract static int Sum(int x, int y);
+        //private abstract static void Main(string[] args);
+    }
+}
